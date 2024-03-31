74b069edc7260b3fa51ffe75c67a131080fdb12a
PhamQuangGiaBao <150901868+PhamQuangGiaBao@users.noreply.github.com>
2024-03-31 12:12:22 +0700

    aaa

--- PhimNew.com/src/main/java/Entity/Video.java
+++ PhimNew.com/src/main/java/Entity/Video.java
@@ -1,5 +1,25 @@
 package Entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Video")
 public class Video {
 
+	@Id
+	@Column(name = "Id")
+	public int Id;
+	
+	@Column(name = "Title")
+	public String Title;
+	
+	@Column(name = "Poster")
+	public String Poster;
+	
+	@Column(name = "Views")
+	public int Views;
+	
 }
